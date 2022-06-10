40e6078931c2496b27516514a8aba7cf3730a016
wallacecardoso <wallacedossantoscardoso@live.com>
2022-06-09 23:36:50 -0300

    Create a class Pessoa

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+    var name: String = "Wallace"
+    var cpf: String = "123.123.123-11"
+}
+
+    fun main(){
+        val wallace = Pessoa()
+
+        println(wallace.name)
+        println(wallace.cpf)
+    }
