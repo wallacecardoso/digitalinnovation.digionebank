bb7259ce7d4796222adff4bab5099c8b45d0b529
wallacecardoso <wallacedossantoscardoso@live.com>
2022-06-10 00:46:31 -0300

    update to talk about class members.

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -5,12 +5,13 @@ class Pessoa {
     var cpf: String = "123.123.123-11"
         private set
 
+    constructor() // consigo criar uma função dentro da class/objeto por conta do constructor().
+    fun pessoaInfo(): String = "${name} e ${cpf}" //interpolação
+
 }
 
     fun main(){
         val wallace = Pessoa()
 
-        println(wallace.name)
-        println(wallace.cpf)
-
+        println(wallace.pessoaInfo()) // Estou fazendo meu objeto Pessoa() chamar sua função pessoaInfo()
     }
