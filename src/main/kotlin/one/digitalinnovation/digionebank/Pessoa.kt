d4a7bf3c1327fbc49be6aa07580271eb5d91c0e7
wallacecardoso <wallacedossantoscardoso@live.com>
2022-06-11 12:53:05 -0300

    update to make person and employee abstract class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,16 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var name: String = "Wallace"
-    var cpf: String = "123.123.123-11"
-        private set
-
-    constructor() // consigo criar uma função dentro da class/objeto por conta do constructor().
-    fun pessoaInfo(): String = "${name} e ${cpf}" //interpolação
-
-}
-
-    fun main(){
-        val wallace = Pessoa()
-        println(wallace.pessoaInfo()) // Estou fazendo meu objeto Pessoa() chamar sua função pessoaInfo()
-    }
+abstract class Pessoa( //Pessoa é formada utilizando um construtor primário e tem suas propriedades marcadas como 'final'.
+   val nome: String,
+   val cpf: String
+)
